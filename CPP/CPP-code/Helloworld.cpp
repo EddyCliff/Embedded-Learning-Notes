9187f2081db87172a71d68b06adf90801ab18a8c
EddyCliff <korgtest1518@gmail.com>
2023-09-09 16:10:06 +0800

    ✨:增加Helloworld.cpp

---
+++ CPP/CPP-code/Helloworld.cpp
@@ -0,0 +1,18 @@
+/********************************************************************************
+* @File name: Helloworld.cpp
+* @Author: EddyCliff
+* @Version: 1.0
+* @Date: 2023-09-09
+* @Description: This is a simple "Hello World" program in C++.
+*               It demonstrates the basic structure of a C++ program.
+********************************************************************************/
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout << "Hello world" << endl; 
+  system("pause");
+  
+  return 0;
+
+}
