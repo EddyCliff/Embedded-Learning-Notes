441f8bb8af26ccddfe46a63d238e99096a12fd77
EddyCliff <korgtest1518@gmail.com>
2023-09-09 16:49:50 +0800

    ✨:增加variable.cpp

---
+++ CPP/CPP-code/variable.cpp
@@ -0,0 +1,18 @@
+/********************************************************************************
+* @File name: variable.cpp
+* @Author: EddyCliff
+* @Version: 1.0
+* @Date: 2023-09-09
+* @Description: This C++ program demonstrates the use of variables.
+*               It initializes an integer variable 'a' with the value 10
+*               and then prints the value to the console.
+********************************************************************************/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    cout << "a = " << a << endl;
+    system("pause");
+    return 0;
+}
